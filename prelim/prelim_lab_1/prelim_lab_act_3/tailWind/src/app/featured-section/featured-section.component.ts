bb83be81b3f1620173d95a5ea1cc247cd5eec7e2
z-akiii <lgdizon@student.hau.edu.ph>
2024-12-04 11:59:03 +0800

    Add files via upload

---
+++ prelim/prelim_lab_1/prelim_lab_act_3/tailWind/src/app/featured-section/featured-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-section.component.html',
+  styleUrl: './featured-section.component.css'
+})
+export class FeaturedSectionComponent {
+
+}
